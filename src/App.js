b594ab3b06fc6bcc6ecb9ad7cc7fe14ce7c4ace6
divyanshsharma2280 <divyansh.sharma.cd.ece22@itbhu.ac.in>
2024-06-04 02:19:14 +0530

    PARTIALLY COMP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import Form from './modules/Form';
 import './App.css';
+import Dashboard from './modules/Dashboard';
+import './index.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+
+const ProtectedRoute = ({ children , auth=false}) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null||false;
+  const currentPath = window.location.pathname;
+
+  if (!isLoggedIn && currentPath !== '/users/sign_up' && auth) {
+    
+    return <Navigate to={'/users/sign_up'} />;
+  } else if (isLoggedIn && (currentPath === '/users/sign_in' || currentPath === '/users/sign_up')) {
+    
+    return <Navigate to={'/'} />;
+  }
+
+  return children;
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={
+        <ProtectedRoute auth={true}>
+          <Dashboard/>
+        </ProtectedRoute>
+      }/>
+      <Route path='/users/sign_in' element={
+        <ProtectedRoute>
+          <Form isSignInPage={true}/>
+        </ProtectedRoute>
+      }/>
+      <Route path='/users/sign_up' element={
+        <ProtectedRoute>
+          <Form isSignInPage={false}/>
+        </ProtectedRoute>
+      }/>
+    </Routes>
   );
 }
 
---
+++ src/modules/Form/index.js
@@ -0,0 +1,59 @@
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+
+const Form = ({
+    isSignInPage = true,
+}) => {
+    const [data, setData] = useState({
+        ...(!isSignInPage &&{
+            fullName: ''
+        }),
+        email: '',
+        password: ''
+    });
+      const navigate = useNavigate()
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`http://localhost:8000/api/${isSignInPage ? 'login': 'register'}`,{
+            method : 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        if(res.status==400){
+            alert('Invalid Credentials')
+        }
+        else{
+            const resData = await res.json()
+            if(resData.token){
+                localStorage.setItem('user:token',resData.token)
+                localStorage.setItem('user:detail',JSON.stringify(resData.user))
+                navigate('/')
+            }
+      }}
+    return (
+        <div className="bg-light h-screen flex items-center justify-center">
+            <div className="bg-white w-[500px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+                <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
+                <div className="text-xl font-light mb-8">{isSignInPage ? 'Sign in to explore' : 'Sign Up to now get started'}</div>
+                <form className="flex flex-col items-center w-full" onSubmit={(e)=> handleSubmit(e)}>
+                {!isSignInPage && <Input label = "Full name" name="name" placeholder="Enter your full name" className="mb-6"
+                value={data.fullName} onChange={(e) => setData({...data,fullName: e.target.value})}/>}
+                <Input label = "Email address" name="email" placeholder="Enter your email" className="mb-6"
+                value={data.email} onChange={(e) => setData({...data,email: e.target.value})}/>
+                <Input label = "password" type='password' name="password" placeholder="Enter your password" className="mb-8"
+                value={data.password} onChange={(e) => setData({...data,password: e.target.value})}/>
+                <Button label={isSignInPage ? "Sign in":"Sign Up"} className="mb-3" type="submit"/>
+                </form>
+                <div>{isSignInPage ? "Don't Have an Account?":"Already have an account?"}<span className="text-primary cursor-pointer underline " onClick={() => navigate(`/users/${isSignInPage ? 'sign_up':'sign_in'}`)}>{isSignInPage ? "Sign up":"Sign in"}</span></div>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
